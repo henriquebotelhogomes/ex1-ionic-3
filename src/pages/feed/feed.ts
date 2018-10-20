7975fbd8371fd9fabe93e8631850af891750d622
henriquebotelhogomes <43866427+henriquebotelhogomes@users.noreply.github.com>
2018-10-19 21:45:57 -0300

    commit2

---
+++ src/pages/feed/feed.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the FeedPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-feed',
+  templateUrl: 'feed.html',
+})
+export class FeedPage {
+  public objeto_feed = {
+    titulo: "Henrique Botelho Gomes",
+    data :"Outubro, 19, 2018",
+    descricao: "Estou criando meu primeiro app IONIC 3",
+    qntd_likes: 12,
+    qntd_comments: 4,
+    time_comment: "11h ago"
+  }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  public somaDoisNumeros(num1:number, num2:number):void{
+    alert(num1+num2);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FeedPage');
+    //this.somaDoisNumeros(2,8);
+  }
+
+}
